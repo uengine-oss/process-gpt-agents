6fc438bafe270aba6c6ce996e4c81e32174e8a0c
kibumpark <top5w5@naver.com>
2025-04-29 10:49:50 +0900

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title>Vue 3 + Vuetify 3</v-card-title>
+                    <v-card-text>
+                        Welcome to your new Vue 3 + Vuetify 3 application!
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeView'
+}
+</script> 
